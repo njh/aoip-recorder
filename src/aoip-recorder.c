11f39cb3ffbff0080c8eeff25ca97a7291a913ad
Nicholas Humfrey <njh@aelius.com>
2018-08-31 13:29:24 +0100

    Added automake build files

---
+++ src/aoip-recorder.c
@@ -0,0 +1,23 @@
+/*
+
+  aoip-recorder.c
+
+  Copyright (C) 2018  Nicholas Humfrey
+  License: MIT
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "config.h"
+
+
+int main(int argc, char *argv[])
+{
+  printf("Hello World\n");
+
+  return EXIT_SUCCESS;
+}
